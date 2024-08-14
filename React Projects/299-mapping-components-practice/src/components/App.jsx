1e40328f89058cbb1a8b3b1e6ae45820b3e5e4af
Abhinav <abhinavsri204@gmail.com>
2024-08-15 02:42:24 +0530

    did emojipedia project by myself as always

--- React Projects/299-mapping-components-practice/src/components/App.jsx
+++ React Projects/299-mapping-components-practice/src/components/App.jsx
@@ -1,52 +1,26 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import emojipedia from "../emojipedia.js"
+import Entry from "./Entry.jsx"
 
 function App() {
   return (
     <div>
+      
       <h1>
         <span>emojipedia</span>
       </h1>
-
       <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
+      {emojipedia.map((contact)=>(
+        <Entry 
+        key={contact.id}
+        name={contact.name}
+        emoji={contact.emoji}
+        meaning = {contact.meaning}
+        />
+      ))}
       </dl>
+
     </div>
   );
 }
