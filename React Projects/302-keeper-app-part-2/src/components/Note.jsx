c4d1943bd05685d068c4176fb0fca790d4c6fe0b
Abhinav <abhinavsri204@gmail.com>
2024-08-17 02:25:23 +0530

    contacts, emojipedia,keeper app part 2 done myself

---
+++ React Projects/302-keeper-app-part-2/src/components/Note.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+export default Note;
